011a2bc5ab43c7b2c9e54d3fe7e33c53ff9abbbe
gabriele <gmercolino2003@gmail.com>
2026-04-06 17:38:22 +0200

    feat: added base module

---
+++ modules/homeManager/default.nix
@@ -0,0 +1,40 @@
+{ self, lib, ... }:
+let
+  modules = self.homeModules;
+in
+{
+  flake.homeModules.default =
+    { config, ... }:
+    let
+      cfg = config.hyprnix;
+    in
+    {
+      imports = with modules; [ ];
+
+      options.hyprnix = {
+        enable = lib.mkEnableOption "hyprnix";
+
+        systemd.enable = lib.mkEnableOption "systemd integration";
+
+        xwayland.enable = lib.mkEnableOption "xwayland";
+
+        extraConfig = lib.mkOption {
+          type = lib.types.lines;
+          default = "";
+          description = ''
+            Extra configuration lines to append to the bottom of
+            `~/.config/hypr/hyprland.conf`.
+          '';
+        };
+      };
+
+      config = lib.mkIf cfg.enable {
+        wayland.windowManager.hyprland = {
+          enable = true;
+          systemd.enable = cfg.systemd.enable;
+          xwayland.enable = cfg.xwayland.enable;
+          extraConfig = cfg.extraConfig;
+        };
+      };
+    };
+}
