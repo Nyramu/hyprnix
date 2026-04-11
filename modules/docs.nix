886662a266b291524e87820721acb1598134f19b
gabriele <gmercolino2003@gmail.com>
2026-04-11 11:33:48 +0200

    feat(docs): now each option has a link to the related file

--- modules/docs.nix
+++ modules/docs.nix
@@ -1,18 +1,43 @@
 { inputs, ... }:
 {
-  perSystem = { pkgs, lib, ... }:
-  let
-    optionsDoc = pkgs.nixosOptionsDoc {
-      options = (lib.evalModules {
-        modules = [
-          inputs.self.homeModules.default
-          { _module.check = false; }
-        ];
-      }).options;
-      transformOptions = opt: opt // { declarations = [ ]; };
-      warningsAreErrors = false;
+  perSystem =
+    { pkgs, lib, ... }:
+    let
+      repoUrl = "https://github.com/Nyramu/hyprnix/blob/main";
+      baseDir = toString inputs.self;
+
+      optionsDoc = pkgs.nixosOptionsDoc {
+        options =
+          (lib.evalModules {
+            modules = [
+              inputs.self.homeModules.default
+              { _module.check = false; }
+            ];
+          }).options;
+
+        warningsAreErrors = false;
+
+        transformOptions =
+          opt:
+          opt
+          // {
+            declarations = map (
+              decl:
+              let
+                declStr = toString decl;
+                # removes some text that would break the link
+                path = builtins.head (lib.splitString "," declStr);
+                rel = lib.removePrefix (baseDir + "/") path;
+              in
+              {
+                name = rel;
+                url = "${repoUrl}/${rel}";
+              }
+            ) opt.declarations;
+          };
+      };
+    in
+    {
+      packages.options-json = optionsDoc.optionsJSON;
     };
-  in {
-    packages.options-json = optionsDoc.optionsJSON;
-  };
 }
