e5d43d3fa7ad2a6625b96bbecb6d0fcbe0cd79ea
Nyramu <107689027+Nyramu@users.noreply.github.com>
2026-04-12 19:21:20 +0200

    feat(xwayland): add module

---
+++ modules/homeManager/xwayland.nix
@@ -0,0 +1,45 @@
+{ lib, ... }:
+{
+  flake.homeModules.xwayland =
+    { config, ... }:
+    let
+      inherit (lib) mkOption;
+      inherit (lib.types)
+        bool
+        nullOr
+        ;
+
+      cfg = config.hyprnix.xwayland;
+    in
+    {
+      options.hyprnix.xwayland = {
+        enable = lib.mkEnableOption "allow running applications using X11";
+
+        use_nearest_neighbor = mkOption {
+          type = nullOr bool;
+          default = null;
+          description = "uses the nearest neighbor filtering for xwayland apps, making them pixelated rather than blurry";
+        };
+
+        force_zero_scaling = mkOption {
+          type = nullOr bool;
+          default = null;
+          description = "forces a scale of 1 on xwayland windows on scaled displays.";
+        };
+
+        create_abstract_socket = mkOption {
+          type = nullOr bool;
+          default = null;
+          description = ''
+            Create the abstract Unix domain socket for XWayland connections.
+            (XWayland restart is required for changes to take effect; Linux only)
+          '';
+        };
+      };
+
+      config = {
+        # Only write actually set values to avoid noise in the file
+        wayland.windowManager.hyprland.xwayland = lib.filterAttrs (_: v: v != null) cfg;
+      };
+    };
+}
