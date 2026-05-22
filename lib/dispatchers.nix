2139a255447f290967125ee45a3e5a0a0f84ba88
gabriele <ciruzzo032@noreply.codeberg.org>
2026-05-22 12:23:04 +0200

    feat(dispatchers): added workspace dispatcher and organized better the rest

--- lib/dispatchers.nix
+++ lib/dispatchers.nix
@@ -5,6 +5,9 @@ let
     optionalString
     concatStringsSep
     filter
+    filterAttrs
+    head
+    attrNames
     ;
   inherit (lib.types)
     nullOr
@@ -16,6 +19,13 @@ let
 
   ifPresent = x: optionalString (x != null);
   luaConcat = fields: concatStringsSep ", " (filter (s: s != "") fields);
+  callNestedBuilders =
+    builders: params:
+    let
+      active = filterAttrs (_: v: v != null) params;
+      name = head (attrNames active);
+    in
+    builders.${name} active.${name};
 
   luaBuilders = {
     exec_cmd = cmd: ''hl.dsp.exec_cmd("${cmd}")'';
@@ -37,7 +47,14 @@ let
         state,
         window,
       }:
-      ''hl.dsp.send_shortcut({mods = "${mods}", key = "${key}", state = "${state}" ${ifPresent window '', window = "${window}"''}})'';
+      "hl.dsp.send_shortcut({${
+        luaConcat [
+          ''mods = "${mods}"''
+          ''key = "${key}"''
+          ''state = "${state}"''
+          (ifPresent window ''window = "${window}"'')
+        ]
+      }})";
     layout = message: ''hl.dsp.layout("${message}")'';
     dpms =
       { action, monitor }:
@@ -46,14 +63,44 @@ let
           (ifPresent action ''action = "${action}"'')
           (ifPresent monitor ''monitor = "${monitor}"'')
         ]
-      })";
+      }})";
     event = message: ''hl.dsp.event("${message}")'';
     global = message: ''hl.dsp.global("${message}")'';
     force_idle = seconds: "hl.dsp.force_idle(${seconds})";
     no_op = _: "hl.dsp.no_op()";
+
+    workspace = callNestedBuilders workspaceBuilders;
   };
 
-  options = {
+  workspaceBuilders = {
+    rename =
+      { workspace, name }:
+      "hl.dsp.workspace.rename({${
+        luaConcat [
+          ''workspace = "${workspace}"''
+          (ifPresent name ''name = "${name}"'')
+        ]
+      }})";
+    move =
+      { workspace, monitor }:
+      "hl.dsp.workspace.move({${
+        luaConcat [
+          (ifPresent workspace ''workspace = "${workspace}"'')
+          ''monitor = "${monitor}"''
+        ]
+      }})";
+    swap_monitors =
+      { monitor1, monitor2 }:
+      "hl.dsp.workspace.swap_monitors({${
+        luaConcat [
+          ''monitor1 = "${monitor1}"''
+          ''monitor2 = "${monitor2}"''
+        ]
+      }})";
+    toggle_special = name: ''hl.dsp.workspace.toggle_special("${name}")'';
+  };
+
+  optionHelpers = {
     simpleStr = mkOption { type = str; };
     nullableStr = mkOption {
       type = nullOr str;
@@ -76,54 +123,65 @@ let
         default = null;
       };
   };
+
+  types = with optionHelpers; {
+    workspaceType = nullableSubmodule {
+      rename = nullableSubmodule {
+        workspace = simpleStr;
+        name = nullableStr;
+      };
+      move = nullableSubmodule {
+        workspace = nullableStr;
+        monitor = simpleStr;
+      };
+      swap_monitors = nullableSubmodule {
+        monitor1 = simpleStr;
+        monitor2 = simpleStr;
+      };
+      toggle_special = nullableStr;
+    };
+
+    send_shortcutType = nullableSubmodule {
+      mods = simpleStr;
+      key = simpleStr;
+      window = simpleStr;
+    };
+
+    send_key_stateType = nullableSubmodule {
+      mods = simpleStr;
+      key = simpleStr;
+      state = mkOption {
+        type = enum [
+          "up"
+          "down"
+        ];
+      };
+      window = nullableStr;
+    };
+
+    dpmsType = nullableSubmodule {
+      action = nullableStr;
+      monitor = nullableStr;
+    };
+  };
 in
 {
   type = submodule {
-    options = with options; {
+    options = with optionHelpers; {
       exec_cmd = nullableStr;
       exec_raw = nullableStr;
       exit = empty;
       submap = nullableStr;
-
-      pass = nullableSubmodule {
-        options.window = simpleStr;
-      };
-
-      send_shortcut = nullableSubmodule {
-        options = {
-          mods = simpleStr;
-          key = simpleStr;
-          window = simpleStr;
-        };
-      };
-
-      send_key_state = nullableSubmodule {
-        options = {
-          mods = simpleStr;
-          key = simpleStr;
-          state = mkOption {
-            type = enum [
-              "up"
-              "down"
-            ];
-          };
-          window = nullableStr;
-        };
-      };
-
+      pass = nullableSubmodule { window = simpleStr; };
+      send_shortcut = types.send_shortcutType;
+      send_key_state = types.send_key_stateType;
       layout = nullableStr;
-
-      dpms = nullableSubmodule {
-        options = {
-          action = nullableStr;
-          monitor = nullableStr;
-        };
-      };
-
+      dpms = types.dpmsType;
       event = nullableStr;
       global = nullableStr;
       force_idle = nullableNumber;
       no_op = empty;
+      workspace = types.workspaceType;
     };
   };
 
